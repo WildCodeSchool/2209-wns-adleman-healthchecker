87517a6e2a5cd911c18806f585ec580959eecb49
Sachastorer <77855451+Sachastorer@users.noreply.github.com>
2022-12-23 16:10:07 +0100

    update url resolver

--- server/src/resolver/UrlResolver.ts
+++ server/src/resolver/UrlResolver.ts
@@ -1,7 +1,8 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import Url, { createUrlInput } from "../entity/Url";
-// import Response from "../entity/Response";
+import Response from "../entity/Response";
 import datasource from "../database";
+import { ApolloError } from "apollo-server-errors";
 
 import { UrlService } from "../services/UrlService";
 
@@ -13,17 +14,43 @@ export class UrlResolver {
   }
 
   @Mutation(() => Url)
-  async createUrl(@Arg("url") url: createUrlInput): Promise<Url> {
+  async createUrl(@Arg("url") url: createUrlInput): Promise<Response> {
     console.log("-------------------------------------");
     console.log(url.url);
 
+    /// VALIDATION URL
+    /// REGEX
+    /// FORMAT
+
+    const urlExist = await datasource
+      .getRepository(Url)
+      .findOneBy({ url: url.url });
+
     const service = new UrlService();
-
-    const response = service.getResponse(url.url);
+    const responseService = await service.getResponse(url.url);
     console.log("La reponse");
-    console.log(response);
+    console.log(responseService);
 
-    // await datasource.getRepository(Response).save();
-    return await datasource.getRepository(Url).save(url);
+    let urlId = null;
+
+    if (urlExist === null) {
+      const urlAdded = await datasource.getRepository(Url).save(url);
+      console.log("URL N'EXISTE PAS");
+      console.log(urlAdded);
+
+      if (urlAdded !== null) urlId = urlAdded.id;
+    }
+
+    if (urlExist !== null) urlId = urlExist.id;
+
+    if (urlId === null) throw new ApolloError("Response not added");
+
+    const response = {
+      url_id: urlId,
+      response_status: responseService.response_status,
+      latency: responseService.latency,
+    };
+
+    return await datasource.getRepository(Response).save(response);
   }
 }
