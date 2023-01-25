22b13cec96ac19a74fa87c693b2144a120e689dd
Sachastorer <77855451+Sachastorer@users.noreply.github.com>
2023-01-25 17:31:19 +0100

    update mobile client

--- client-mobile/App.tsx
+++ client-mobile/App.tsx
@@ -5,11 +5,44 @@ import { ApolloProvider } from "@apollo/client";
 import client from "./graphql/client";
 import { useEffect } from "react";
 import UrlList from "./components/UrlList";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
       <UrlList />
+      {/* <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Camera") {
+                iconName = focused ? "camera" : "camera-outline";
+              } else if (route.name === "Images") {
+                iconName = focused ? "image" : "image-outline";
+              } else if (route.name === "Feed") {
+                iconName = focused ? "share-social" : "share-social-outline";
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "blue",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen
+            name="Camera"
+            component={CameraScreen}
+            options={{ unmountOnBlur: true }}
+          />
+          <Tab.Screen name="Images" component={ImageScreen} />
+          <Tab.Screen name="Feed" component={FeedScreen} />
+        </Tab.Navigator>
+      </NavigationContainer> */}
     </ApolloProvider>
   );
 }
