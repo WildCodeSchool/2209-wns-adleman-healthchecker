03d7995aa026702fbf48e11eebe5feb74c262615
Sachastorer <77855451+Sachastorer@users.noreply.github.com>
2023-01-27 11:56:19 +0100

    update mobile client

--- client-mobile/components/Screen/HomeScreen.tsx
+++ client-mobile/components/Screen/HomeScreen.tsx
@@ -1,6 +1,16 @@
-import { StyleSheet, Text, View, FlatList, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TextInput,
+} from "react-native";
+import { useState, useEffect } from "react";
 import { Url, useGetUrlsQuery } from "../../graphql/generated/schema";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useCreateUrlMutation } from "../../graphql/generated/schema";
+import { Response } from "../../graphql/generated/schema";
 
 type RootStackParamList = {
   Home: undefined;
@@ -10,28 +20,95 @@ type RootStackParamList = {
 type HomeProps = NativeStackScreenProps<RootStackParamList, "Home", "Tab">;
 
 export default function HomeScreen({ route, navigation }: HomeProps) {
+  const [url, setUrl] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [response, setResponse] = useState<Response | undefined>(undefined);
+
+  const [createUrl] = useCreateUrlMutation();
+
   const { data, refetch } = useGetUrlsQuery();
   const urlList = data?.getUrls;
+
+  useEffect(() => {
+    const urlPattern =
+      /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+
+    const reg = new RegExp(urlPattern);
+    if (url.length === 0) {
+      return;
+    }
+    if (!reg.test(url)) {
+      setIsValid(false);
+      setIsDisabled(true);
+    } else {
+      setIsValid(true);
+      setIsDisabled(false);
+    }
+  }, [url]);
 
   const navigateToHistorical = (url: Url) => {
     navigation.navigate("Historical", { url: url });
   };
 
+  const handleChangeUrl = (url: string) => {
+    setUrl(url);
+    setResponse(undefined);
+  };
+
+  const handleValidate = async () => {
+    if (isValid && !isDisabled) {
+      try {
+        const res = await createUrl({ variables: { url: { url } } });
+        setResponse(res?.data?.createUrl?.responses[0]);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        refetch();
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <FlatList
-        keyExtractor={(item: any) => item.id}
-        contentContainerStyle={{ paddingBottom: 30 }}
-        // ItemSeparatorComponent={() => <View style={style.separator} />}
-        // ListEmptyComponent={() => <Text>No wilders for now</Text>}
-        data={urlList}
-        // refreshing={loadingWilders}
-        renderItem={({ item }) => (
-          <View style={styles.container}>
-            <Text onPress={() => navigateToHistorical(item)}>{item.url}</Text>
-          </View>
-        )}
-      />
+      <View style={styles.containerInput}>
+        <Text>Enter URL : </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="http://toto.fr"
+          onChangeText={(url) => handleChangeUrl(url)}
+          value={url}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Button
+          onPress={handleValidate}
+          title="Validate"
+          disabled={isDisabled || !isValid}
+        />
+      </View>
+      <View>
+        <Text>
+          Status: {response?.response_status && response.response_status}
+        </Text>
+        <Text>Latency: {response?.latency && response.latency}</Text>
+        <Text>Date: {response?.created_at && response.created_at}</Text>
+      </View>
+      <View style={styles.containerList}>
+        <FlatList
+          keyExtractor={(item: any) => item.id}
+          contentContainerStyle={{ paddingBottom: 30 }}
+          // ItemSeparatorComponent={() => <View style={style.separator} />}
+          // ListEmptyComponent={() => <Text>No wilders for now</Text>}
+          data={urlList}
+          // refreshing={loadingWilders}
+          renderItem={({ item }) => (
+            <View>
+              <Text onPress={() => navigateToHistorical(item)}>{item.url}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -42,5 +119,28 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    borderColor: "red",
+    borderWidth: 1,
+  },
+  containerInput: {
+    backgroundColor: "#fff",
+    borderColor: "blue",
+    borderWidth: 1,
+  },
+  containerList: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    borderColor: "green",
+    borderWidth: 1,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    width: 200,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
   },
 });
