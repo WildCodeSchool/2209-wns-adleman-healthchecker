1902f0eba532ccb83865965d64e2be45ca9fee26
Sachastorer <s.storer@sporting-comm.fr>
2023-06-22 15:17:58 +0200

    improve periodic fetch function

--- server/src/services/UrlService.ts
+++ server/src/services/UrlService.ts
@@ -184,4 +184,48 @@ export class UrlService {
     }
     return responseForUrl;
   }
+
+  hasToBeFetched = (freq: number, lastresp: number): boolean => {
+    const now = new Date().getTime();
+    return now - lastresp > freq;
+  };
+
+  async periodicFetchUrl(): Promise<void> {
+    const urlService = new UrlService();
+    const interval = 5000;
+    const update = async (): Promise<void> => {
+      const _urls = await datasource
+        .getRepository(Url)
+        .find({ relations: ["userToUrls"] });
+
+      const urls = await Promise.all(
+        _urls.map(async (u: Url) => {
+          const response = await datasource
+            .getRepository(Response)
+            .find({ where: { urlId: u.id }, take: 1, order: { id: "DESC" } });
+          return { ...u, response };
+        })
+      );
+
+      urls.forEach(async (url) => {
+        let minFrequency = 3600000;
+        if (url.userToUrls.length > 0)
+          minFrequency = url.userToUrls.sort(
+            (a, b) => a.frequency - b.frequency
+          )[0].frequency;
+        if (minFrequency !== url.frequency) {
+          url.frequency = minFrequency;
+          await datasource.getRepository(Url).save(url);
+        }
+
+        const lastResSecond = url.response[0].created_at.getTime();
+
+        if (this.hasToBeFetched(minFrequency, lastResSecond)) {
+          console.log(`${url.url} IS FETCHED`);
+          await urlService.getAndSaveResponseForEachUrl(url.url, url.id);
+        }
+      });
+    };
+    setInterval(update, interval);
+  }
 }
